6b72ce8f5240f4a31c97fa97d2f1b035f1ad193d
Rana Paul <ranapaul741223@gmail.com>
2024-04-16 11:31:41 +0530

    connect to wallet and gat balance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+import { Web3Modal } from '../context/web3model'
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Web3Modal>{children}</Web3Modal>
+      </body>
     </html>
   );
 }
---
+++ utils/providers.ts
@@ -0,0 +1,3 @@
+import {ethers} from 'ethers'
+const provider = new ethers.JsonRpcProvider('https://sepolia.infura.io/v3/c640f8825de74a38a1874cafa580a98d');
+
